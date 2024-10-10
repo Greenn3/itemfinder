ea45f4e9d70a0459f6388333b8eae2aed485a929
Greenn3 <96129501+Greenn3@users.noreply.github.com>
2024-10-10 18:52:42 +0200

    The begining

---
+++ ItemFinder_Java/src/test/java/com/example/itemfinder_java/ItemFinderJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itemfinder_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemFinderJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
